054dd5f156efa3753bd2005646259cf066c50e86
barakov14 <playmagma4@gmail.com>
2024-08-22 23:09:04 +0500

    input directive implemented

---
+++ src/app/shared/directives/input/input.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, ElementRef, inject, OnInit, Renderer2} from "@angular/core";
+
+@Directive({
+  selector: '[stInput]',
+  standalone: true
+})
+
+export class InputDirective implements OnInit {
+
+  private readonly renderer = inject(Renderer2)
+  private readonly elementRef = inject(ElementRef)
+
+  ngOnInit() {
+    const el = this.elementRef.nativeElement
+
+    this.renderer.setStyle(el, 'padding', '8px 12px');
+    this.renderer.setStyle(el, 'font-size', '16px');
+    this.renderer.setStyle(el, 'border', '1px solid #ccc');
+    this.renderer.setStyle(el, 'border-radius', '4px');
+    this.renderer.setStyle(el, 'outline', 'none');
+    this.renderer.setStyle(el, 'transition', 'border-color 0.3s ease');
+  }
+}
