a9c7eb62319384ac766cb42977a9b9cfc88122d7
barakov14 <playmagma4@gmail.com>
2024-08-22 23:10:11 +0500

    progress spinner created

---
+++ src/app/shared/components/progress-spinner/progress-spinner.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'st-progress-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './progress-spinner.component.html',
+  styleUrl: './progress-spinner.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProgressSpinnerComponent {
+
+}
